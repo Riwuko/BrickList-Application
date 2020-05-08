55ec9a084c99be5817223f809d15ff20068f97eb
riwuko x <riwuko@gmail.com>
2020-05-08 14:54:16 +0200

    Basic activities layout created

---
+++ app/src/main/java/com/example/bricklistapplication/IsProjectActiveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bricklistapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IsProjectActiveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_is_project_active)
+    }
+}
---
+++ app/src/main/java/com/example/bricklistapplication/PackageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bricklistapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PackageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package)
+    }
+}
